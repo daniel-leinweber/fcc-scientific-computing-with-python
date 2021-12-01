0075557be3fd7c65540305d548c2d4d313c5d9f4
Daniel Leinweber <daniel.leinweber@maxdecor.de>
2021-12-01 08:20:57 +0100

    Polygon Area Calculator

---
+++ polygon-area-calculator/shape_calculator.py
@@ -0,0 +1,48 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+  
+  def set_width(self, width):
+    self.width = width
+
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    return self.width * self.height
+  
+  def get_perimeter(self):
+    return 2 * (self.width + self.height)
+  
+  def get_diagonal(self):
+    return (self.width ** 2 + self.height ** 2) ** 0.5
+  
+  def get_picture(self):
+    if self.width > 50 or self.height > 50:
+      return 'Too big for picture.'
+    
+    return ('*' * self.width + '\n') * self.height
+
+  def get_amount_inside(self, shape):
+    return self.get_area() // shape.get_area()
+
+  def __str__(self):
+    return f'Rectangle(width={self.width}, height={self.height})'
+
+class Square(Rectangle):
+  def __init__(self, side_length):
+    super().__init__(side_length, side_length)
+  
+  def set_side(self, side_length):
+    self.width = side_length
+    self.height = side_length
+  
+  def __str__(self):
+    return f'Square(side={self.width})'
+  
+  def set_width(self, width):
+    self.set_side(width)
+  
+  def set_height(self, height):
+    self.set_side(height)
